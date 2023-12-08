2fc064bf0d7817b779885707cfdd2ca92ff1ae6d
zhoudm5 <zhoudm5@sany.com.cn>
2023-12-08 17:30:45 +0800

    Update version to 0.11.51

--- moduleA/index.js
+++ moduleA/index.js
@@ -1,4 +1,16 @@
-const TARGET = process.env.npm_lifecycle_event;
-console.log('TARGET', TARGET)
-console.log('process.env.PORT',  process.env.PORT)
-console.log('process.argv', process.argv)
+const fs = require('fs');
+const child_process = require('child_process');
+
+// 获取当前版本号
+const currentVersion = JSON.parse(fs.readFileSync('package.json')).version;
+
+console.log('currentVersion', currentVersion)
+
+const gitCmd = `git add . && git commit -m "Update version to ${currentVersion}" && git tag -a v${currentVersion} -m "version ${currentVersion}" && git push --tags`;
+child_process.exec(gitCmd, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`执行 git 命令时出错: ${error}`);
+    return;
+  }
+  console.log(`成功推送至远程仓库并打上标签：${stdout}`);
+});
