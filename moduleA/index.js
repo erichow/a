ff9b1c5490ec86bd3a1b9f2698aca5b0e677f59d
zhoudm5 <zhoudm5@sany.com.cn>
2023-12-08 17:37:14 +0800

    test

--- moduleA/index.js
+++ moduleA/index.js
@@ -1,16 +1,15 @@
-const fs = require('fs');
 const child_process = require('child_process');
-
-// 获取当前版本号
 const currentVersion = JSON.parse(fs.readFileSync('package.json')).version;
-
-console.log('currentVersion', currentVersion)
-
-const gitCmd = `git add . && git commit -m "Update version to ${currentVersion}" && git tag -a v${currentVersion} -m "version ${currentVersion}" && git push --tags`;
+const gitCmd = `
+	git add . && 
+	git commit -m "Update version to ${currentVersion}" && 
+	git tag v${currentVersion} && 
+	git push --tags
+`;
 child_process.exec(gitCmd, (error, stdout, stderr) => {
   if (error) {
     console.error(`执行 git 命令时出错: ${error}`);
     return;
   }
-  console.log(`成功推送至远程仓库并打上标签：${stdout}`);
+  console.log(`成功推送至远程仓库并打上标签： v${currentVersion}`);
 });
