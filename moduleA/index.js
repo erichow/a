64c92c3d2bd813e65cafaadd5da432d28336a5f1
zhoudm5 <zhoudm5@sany.com.cn>
2023-12-08 17:46:16 +0800

    更新组件版本 @hxdms/a@0.11.58

--- moduleA/index.js
+++ moduleA/index.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const child_process = require('child_process');
 const pkg = JSON.parse(fs.readFileSync('package.json'));
 const currentVersion = `${pkg.name}@${pkg.version}`;
-const gitCmd = `git add . && git commit -m "更新组件版本 v${currentVersion}" && git tag v${currentVersion} && git push --tags`;
+const gitCmd = `git add . && git commit -m "更新组件版本 ${currentVersion}" && git tag ${currentVersion} && git push --tags`;
 child_process.exec(gitCmd, (error, stdout, stderr) => {
   if (error) {
     console.error(`执行 git 命令时出错: ${error}`);
     return;
   }
-  console.log(`成功推送至远程仓库并打上标签： v${currentVersion}`);
+  console.log(`成功推送至远程仓库并打上标签： ${currentVersion}`);
 });
