915a3b67e3ddc4702920a26186bc8c8f2663117b
zhoudm5 <zhoudm5@sany.com.cn>
2023-12-15 14:55:48 +0800

    更新组件版本 @hxdms/a@0.15.1-DDM3514-SNAPSHOT.6

--- moduleA/build/genver.js
+++ moduleA/build/genver.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const rawArgv = process.argv.slice(2);
 const args = rawArgv.join(' ')
 const child_process = require('child_process');
-let npmCmd = `npm version prerelease --preid ${args}`
 
-child_process.exec(npmCmd, (error, stdout, stderr) => {
+// 更新版本号
+child_process.exec(`npm version prerelease --preid ${args}`, (error, stdout, stderr) => {
   if (error) {
     console.error(`执行 git 命令时出错: ${error}`);
     return;
@@ -12,8 +12,8 @@ child_process.exec(npmCmd, (error, stdout, stderr) => {
   const pkg = JSON.parse(fs.readFileSync('package.json'));
   const currentVersion = `${pkg.name}@${pkg.version}`;
 
-  npmCmd = `npm publish && npm dist-tag add ${currentVersion} ${args || 'snapshot'}`;
-  console.log(npmCmd)
+  // 推送npm仓库
+  const npmCmd = `npm publish && npm dist-tag add ${currentVersion} ${args || 'snapshot'}`;
   child_process.exec(npmCmd, (error, stdout, stderr) => {
     if (error) {
       console.error(`执行 git 命令时出错: ${error}`);
@@ -22,7 +22,7 @@ child_process.exec(npmCmd, (error, stdout, stderr) => {
     console.log(`成功推送至NPM仓库： ${currentVersion}`);
   });
 
-
+  // 推送git仓库并打上tag标签
   const gitCmd = `git add . && git commit -m "更新组件版本 ${currentVersion}" && git tag ${currentVersion} && git push --tags`;
   child_process.exec(gitCmd, (error, stdout, stderr) => {
     if (error) {
